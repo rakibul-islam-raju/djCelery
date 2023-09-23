1ab764bc82f20885c5e560b1a85d9255857c0bfc
Rakibul Islam <rakibul.islam7772588@gmail.com>
2023-09-23 22:57:14 +0600

    celery task priority

--- djCelery/djCelery/celery.py
+++ djCelery/djCelery/celery.py
@@ -4,8 +4,18 @@ from celery import Celery
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djCelery.settings")
 app = Celery("djCelery")
 app.config_from_object("django.conf:settings", namespace="CELERY")
-app.conf.task_routes = {
-    "newapp.tasks.task1": {"queue": "queue1"},
-    "newapp.tasks.task2": {"queue": "queue2"},
+
+
+# routing
+# app.conf.task_routes = {
+#     "newapp.tasks.task1": {"queue": "queue1"},
+# }
+
+# Task Prioritize
+app.conf.broker_transport_options = {
+    "priority_steps": list(range(10)),
+    "sep": ":",
+    "queue_order_strategy": "priority",
 }
+
 app.autodiscover_tasks()
--- djCelery/newapp/tasks.py
+++ djCelery/newapp/tasks.py
@@ -1,11 +1,26 @@
+import time
 from celery import shared_task
 
 
 @shared_task
-def task1():
-    return
+def tp1(queue="celery"):
+    time.sleep(15)
+    return "tp1"
 
 
 @shared_task
-def task2():
-    return
+def tp2(queue="celery:1"):
+    time.sleep(12)
+    return "tp2"
+
+
+@shared_task
+def tp3(queue="celery:2"):
+    time.sleep(5)
+    return "tp3"
+
+
+@shared_task
+def tp4(queue="celery:3"):
+    time.sleep(3)
+    return "tp4"
