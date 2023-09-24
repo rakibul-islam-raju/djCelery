f73bed46669d8b2a25b0419050a433ac9bae6437
Rakibul Islam <rakibul.islam7772588@gmail.com>
2023-09-24 11:35:34 +0600

    updated celery settings

--- djCelery/djCelery/celery.py
+++ djCelery/djCelery/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from kombu import Queue, Exchange
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djCelery.settings")
 app = Celery("djCelery")
@@ -12,10 +13,24 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 # }
 
 # Task Prioritize
-app.conf.broker_transport_options = {
-    "priority_steps": list(range(10)),
-    "sep": ":",
-    "queue_order_strategy": "priority",
-}
+# app.conf.broker_transport_options = {
+#     "priority_steps": list(range(10)),
+#     "sep": ":",
+#     "queue_order_strategy": "priority",
+# }
+
+app.conf.task_queues = [
+    Queue(
+        "tasks",
+        Exchange("tasks"),
+        routing_key="tasks",
+        queue_arguments={"x-max-priority": 10},
+    )
+]
+
+app.conf.task_acks_late = True
+app.conf.task_default_priority = 5
+app.conf.worker_prefetch_multiplier = 1
+app.conf.worker_concurrency = 1
 
 app.autodiscover_tasks()
